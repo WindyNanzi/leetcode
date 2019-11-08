cd5d610566e0a956ce83631cddf3ae024f048e56
hanqiao <hanqiao@lenovocloud.com>
2019-11-08 15:21:11 +0800

    bug: 新增深拷贝的递归实现，但同样无法彻底解决新重复对象的窘境

--- 栈/5.克隆图.js
+++ 栈/5.克隆图.js
@@ -55,50 +55,90 @@ b.neighbors.push(c,a)
 c.neighbors.push(d,b)
 d.neighbors.push(a,c)
 
-// cloneGraph(a)
+cloneGraph(a)
 
-// 不依赖具体属性的深拷贝，但仍然需要使用到Map映射
-// var cloneGraph1 = function (node) {
-//   const o = {},
-//         srcQueue = [],
-//         desQueue = [],
-//         map = new Map()
+//不依赖具体属性的深拷贝，但仍然需要使用到Map映射,非递归形式，但仍然不能通过测试，原因在于有的对象未被重复使用
+var cloneGraph1 = function (node) {
+  const o = {},
+        srcQueue = [],
+        desQueue = [],
+        map = new Map()
 
-//   srcQueue.push(node)
-//   desQueue.push(o)
-//   while(srcQueue.length !== 0){
-//     let temp = srcQueue.shift(),
-//         obj = desQueue.shift()
+  srcQueue.push(node)
+  desQueue.push(o)
+  while(srcQueue.length !== 0){
+    let temp = srcQueue.shift(),
+        obj = desQueue.shift()
     
-//     for(const key in temp){
-//       if(Object.prototype.hasOwnProperty.call(temp,key)){
-//         let prop = temp[key]
-//         let type = typeof prop
-//         switch(type){
-//           case 'object':
-//             if(map.has(prop)){
-//               obj[key] = map.get(prop)
-//               break
-//             }
-//             if(Array.isArray(prop)){
-//               obj[key] = [] 
-//             } else {
-//               obj[key] = {}
-//             }
-//             srcQueue.push(prop)
-//             desQueue.push(obj[key])
-//             break
-//           default:
-//             obj[key] = prop
-//         }
-//       } else {
-//         obj.__proto__[key] =  prop
-//       }
-//     }
-//     if(!Array.isArray(temp) && !map.has(temp)) map.set(temp, obj)
-//   }
-//   console.log(o)
-//   return o
-// }
+    for(const key in temp){
+      if(Object.prototype.hasOwnProperty.call(temp,key)){
+        let prop = temp[key]
+        let type = typeof prop
+        switch(type){
+          case 'object':
+            if(map.has(prop)){
+              obj[key] = map.get(prop)
+              break
+            }
+            if(Array.isArray(prop)){
+              obj[key] = [] 
+            } else {
+              obj[key] = {}
+            }
+            srcQueue.push(prop)
+            desQueue.push(obj[key])
+            break
+          default:
+            obj[key] = prop
+        }
+      } else {
+        obj.__proto__[key] =  prop
+      }
+    }
+    if(!Array.isArray(temp) && !map.has(temp)) map.set(temp, obj)
+  }
+  console.log(o)
+  return o
+}
 
-// cloneGraph1(a)
+cloneGraph1(a)
+
+// 采用了递归形式的深拷贝。同样无法通过测试，原因同上
+var cloneGraph2 = function (node){
+  const o = Object.create(null),
+        visited = new Map()
+  const _clone = (src,dis) => {
+    for(const key in src){
+      if(Object.prototype.hasOwnProperty.call(src,key)){
+        let prop = src[key]
+        let type = typeof prop
+        switch(type){
+          case 'object':
+            if(visited.has(prop)){
+              dis[key] = visited.get(prop)
+              break
+            }
+            if(Array.isArray(prop)){
+              dis[key] = []
+            }else{
+              dis[key] = Object.create(null)
+            }
+            if(!visited.has(prop)){
+              if(!Array.isArray(prop)) {visited.set(prop, dis[key])}
+              _clone(prop, dis[key])
+            }
+            break
+          default:
+            dis[key] = prop
+        }
+      } else {
+        dis.__proto__[key] = src[key]
+      }
+    }
+  }
+  _clone(node, o)
+  console.log(o)
+  return o
+}
+
+cloneGraph2(a)
