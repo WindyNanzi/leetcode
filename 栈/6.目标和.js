1c177c6c7423a5a82b0e45c33fa38626aaef7299
hanqiao <hanqiao@lenovocloud.com>
2019-11-12 14:56:53 +0800

    wip: 利用0-1背包解决该问题虽然已经借鉴解法，但是未能参透其原理，暂时告一段落

--- 栈/6.目标和.js
+++ 栈/6.目标和.js
@@ -23,5 +23,38 @@ var findTargetSumWays = function(nums, S) {
   return count
 };
 
-findTargetSumWays([0,0,0,0,0,0,0,1],1)
-findTargetSumWays([42,24,30,14,38,27,12,29,43,42,5,18,0,1,12,44,45,50,21,47],38)
+// findTargetSumWays([0,0,0,0,0,0,0,1],1)
+// findTargetSumWays([42,24,30,14,38,27,12,29,43,42,5,18,0,1,12,44,45,50,21,47],38)
+
+//  背包解决
+var findTargetSumWays1 = function(nums, S){
+  let sum = nums.reduce((a, b) => a+b)
+  if((sum < S) || (sum + S)%2 !== 0){
+    return 0
+  }
+  let P = (sum + S) / 2
+  let dp = []
+  for(let i = 0; i < P ; i++){
+    dp.push(0)
+  }
+  // dp 主要是存储组成方法的数量，dp[0] = 1 即为组合成0的方法，只有1种，就是什么也不取。
+  // dp[5] = 3 代表使总和加到5总共有3种方法
+  // 当背包容量为0时，什么也不做，就满足背包满了的条件，所以可行解至少为1
+  dp.unshift(1) //[1,0,0,...]
+  
+  // 问题转换为 在 num 数组中，有几种加法可以凑出和为 P 的背包问题
+  // 一个容量为 P 的背包，num数组中的数值 中有几种塞法让其塞满
+  debugger
+  for(const num of nums){
+    let cur = P
+    for(cur; cur >= num; cur--){
+      dp[cur] += dp[cur - num]
+    }
+  }
+
+  console.log(dp[P])
+  return dp[P]
+}
+
+// findTargetSumWays1([42,24,30,14,38,27,12,29,43,42,5,18,0,1,12,44,45,50,21,47],38)
+findTargetSumWays1([2,3,4,5,6],18)
