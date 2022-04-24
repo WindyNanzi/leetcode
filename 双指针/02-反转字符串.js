cacde7e30b2b5e64d96fe9adc9a3c2180f9eeda4
hanqiao <hanqiao1024@163.com>
2022-04-24 09:35:03 +0800

    feat: 调整一下代码，采用解构赋值

--- 双指针/02-反转字符串.js
+++ 双指针/02-反转字符串.js
@@ -26,12 +26,9 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-  let i = 0, j = s.length - 1, temp = null
-  
+  let i = 0, j = s.length - 1
   while(i < j) {
-    temp = s[i]
-    s[i] = s[j]
-    s[j] = temp
+    [s[i], s[j]] = [s[j], s[i]]
     i++
     j--
   }
